3dbbdb765ca64a48f75837ee92b14710e988f6e4
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-26 12:57:28 -0400

    model update added false as default

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
 	var Burger = sequelize.define("Burger", {
-		burger_name: DataTypes.STRING,
-		devoured: DataTypes.BOOLEAN
+		burger_name: {
+			type: DataTypes.STRING,
+			//require a text value input
+			allowNull: false
+		},
+		devoured: {
+			type: DataTypes.BOOLEAN,
+			//default it to false
+			defaultValue: false
+		}
 	});
 	return Burger;
 	};
