d85ed48458a4094ba32a0aa6f84c45be28e10dfd
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-26 12:47:47 -0400

    burger model

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,7 @@
-//import the ORM to create functions that will interact with the database 
-var orm = require("../config/orm.js");
-
-var burger = {
-	all: function(cb) {
-		orm.all("burgers", function (res) {
-			cb(res);
-		});
-	},
-	//variables cols and vals are arrays
-	create: function(cols, vals, cb) {
-		orm.create("burgers", cols, vals, function (res) {
-			cb(res);
-		});
-	},
-	update: function(objColVals, condition, cb) {
-		orm.update("burgers", objColVals, condition, function (res) {
-			cb(res);
-		});
-	}
-};
-
-module.exports = burger;
-	
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+		burger_name: DataTypes.STRING,
+		devoured: DataTypes.BOOLEAN
+	});
+	return Burger;
+	};
