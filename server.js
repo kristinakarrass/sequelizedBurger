daa337c8fe4940217dde7de3d2603603af23c315
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-26 12:53:28 -0400

    server update

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+//require models for syncing
+var db = require("./models");
+
 //Serve static conent for the app from the "public" directory in the application directory
 app.use(express.static(process.cwd() + "/public"));
 
@@ -25,4 +28,11 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(PORT);
+require("./routes/html-routes.js");
+require("./routes/api-routes.js");
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+    	console.log("App listening on PORT " + PORT);
+    });	
+});
