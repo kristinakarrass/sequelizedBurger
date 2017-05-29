fb723befd578dd86faaa7af34f8f78d399baea2f
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-28 20:52:25 -0400

    api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,11 @@ var db = require("../models");
 module.exports = function(app) {
 	//GET route for getting all burgers (devoured and not devoured)
 	app.get("/api/burgers", function(req, res) {
-
+		//return all entries from burger table
+		db.Burger.findAll({}).then(function(dbBurger) {
+			//all burgers can be accessed as an argument in the callback function
+			res.json(dbBurger);
+		});
 	});
 	//POST route for saving a new burger
 	app.post("/api/burgers", function(req, res) {
